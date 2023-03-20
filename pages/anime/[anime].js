a8298ff8453c2e4a3f97d1f3048bb4b6155a4e6b
ziwei531 <59821731+ziwei531@users.noreply.github.com>
2023-03-20 23:41:06 +0800

    temp disble [anime].js

--- pages/anime/[anime].js
+++ pages/anime/[anime].js
@@ -6,27 +6,27 @@ export default function Description() {
 	);
 }
 
-export async function getStaticProps() {
-	let searchQuery = "Blue Lock";
-	const query = `
-	query {
-		Media (search: "${searchQuery}", type: ANIME) {
-		  title {
-			romaji
-		  }
-		  description
-		}
-	  }
-	`;
+// export async function getStaticProps() {
+// 	let searchQuery = "Blue Lock";
+// 	const query = `
+// 	query {
+// 		Media (search: "${searchQuery}", type: ANIME) {
+// 		  title {
+// 			romaji
+// 		  }
+// 		  description
+// 		}
+// 	  }
+// 	`;
 
-	// const response = await fetch("https://learnwebcode.github.io/json-example/posts.json")
-	const data = await request("https://graphql.anilist.co", query);
+// 	// const response = await fetch("https://learnwebcode.github.io/json-example/posts.json")
+// 	const data = await request("https://graphql.anilist.co", query);
 
-	console.log(data.Media);
+// 	console.log(data.Media);
 
-	return {
-		props: {
-			anime: data.Media,
-		},
-	};
-}
+// 	return {
+// 		props: {
+// 			anime: data.Media,
+// 		},
+// 	};
+// }
