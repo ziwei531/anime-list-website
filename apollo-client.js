7d2ad45b4fec61f44fbb210de4f255981c7b1e8c
ziwei531 <59821731+ziwei531@users.noreply.github.com>
2023-03-24 16:12:03 +0800

    update apollo-client

--- apollo-client.js
+++ apollo-client.js
@@ -7,40 +7,7 @@ import { offsetLimitPagination } from "@apollo/client/utilities";
 
 const client = new ApolloClient({
 	uri: "https://graphql.anilist.co",
-	cache: new InMemoryCache({
-		typePolicies: {
-			Query: {
-				fields: {
-					Page: {
-						keyArgs: ["sort", "type"],
-						merge(existing = {}, incoming, { args }) {
-							const merged = existing ? { ...existing } : {};
-							const { media: existingMedia = [] } = merged;
-							const { media: incomingMedia = [] } = incoming;
-							const mergedMedia = [...existingMedia];
-							const idSet = new Set(existingMedia.map((media) => media.__ref));
-							incomingMedia.forEach((media) => {
-								if (!idSet.has(media.__ref)) {
-									idSet.add(media.__ref);
-									mergedMedia.push(media);
-								}
-							});
-							merged.media = mergedMedia;
-							return merged;
-						},
-					},
-				},
-			},
-		},
-		dataIdFromObject: (object) => {
-			switch (object.__typename) {
-				case "Media":
-					return object.id;
-				default:
-					return defaultDataIdFromObject(object);
-			}
-		},
-	}),
+	cache: new InMemoryCache(),
 });
 
 export default client;
