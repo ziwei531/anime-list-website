022142f9a015df12660bcce156a0f52625be74c5
Whoong Zi Wei <59821731+ziwei531@users.noreply.github.com>
2023-04-03 11:02:46 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,32 +2,15 @@ import { useState } from "react";
 import AnimeList from "../components/AnimeList";
 import ClientOnly from "../components/ClientOnly";
 import SelectList from "../components/SelectList";
-import AnimeSearch from "../components/AnimeSearch";
 
 export default function Home({ search }) {
 	const [selection, setSelection] = useState("TRENDING_DESC");
-	// console.log("selection: " + selection);
 
-	// if (search) {
-	// 	setSearchTrue(true);
-	// } else {
-	// 	searchFalse;
-	// }
-
-	const [searchTrue, setSearchTrue] = useState(false);
-
-	// console.log(search);
 	return (
 		<>
 			<ClientOnly>
 				<SelectList selection={setSelection} />
-
-				{!searchTrue ? (
-					<AnimeList selection={selection} />
-				) : (
-					<AnimeSearch search={search} />
-				)}
-				{/* <AnimeList selection={selection} /> */}
+				<AnimeList selection={selection} />
 			</ClientOnly>
 		</>
 	);
