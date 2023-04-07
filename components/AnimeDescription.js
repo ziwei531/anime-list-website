c97fb5f381d33cd575f569d55d65ff93eef36877
Whoong Zi Wei <59821731+ziwei531@users.noreply.github.com>
2023-04-08 00:29:03 +0800

    Update AnimeDescription.js

--- components/AnimeDescription.js
+++ components/AnimeDescription.js
@@ -20,7 +20,7 @@ export default function AnimeDescription({ data }) {
 				/>
 				<div className="pt-10 pb-10 pl-5 pr-5 md:pl-20 md:pr-20">
 					<h1 className="text-3xl pb-10">{data.Media.title.userPreferred}</h1>
-					<h1 className="text-2xl">Description</h1>
+					<h2 className="text-2xl">Description</h2>
 					<p className="leading-8">{ReactHtmlParser(data.Media.description)}</p>
 					<div className="flex flex-col md:flex-row text-center mt-5 md:items-start">
 						<span
