7a0a6aac4751818aa85965b26f85c529e128c1eb
Whoong Zi Wei <59821731+ziwei531@users.noreply.github.com>
2023-04-07 23:13:10 +0800

    added meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -5,12 +5,21 @@ import { ApolloProvider } from "@apollo/client";
 import client from "../apollo-client";
 import Footer from "../components/Footer";
 import { useState } from "react";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
 	const [search, setSearch] = useState("");
 	return (
 		<ApolloProvider client={client}>
 			<ThemeProvider>
+				<Head>
+					<title>Simple Anime List Application</title>
+					<meta
+						name="description"
+						content="A Simple Anime List Application. Developed by Whoong Zi Wei"
+						key="desc"
+					/>
+				</Head>
 				<Navbar search={setSearch} />
 				<Component {...pageProps} search={search} />
 				<Footer />
